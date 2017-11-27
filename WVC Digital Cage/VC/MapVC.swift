9acc95bf8818fdfdb134189a42ce149ba4ec0e9f
brian bird <birdsoftware@gmail.com>
2017-11-27 11:30:34 -0800

    updated map view

--- WVC Digital Cage/VC/MapVC.swift
+++ WVC Digital Cage/VC/MapVC.swift
@@ -13,49 +13,70 @@ class MapVC: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var mapView: UIView!
     
+    @IBOutlet var singleButtons: [UIButton]!
+    //S 1-8  tag
+    //D 9-16,17-24
+    //T 25-37
+    //I 38-41
+    var kennelIntArray = ["S1","S2","S3","S4","S5","S6","S7","S8",
+                          "D1","D2","D3","D4","D5","D6","D7","D8","D9","D10",
+                          "D11","D12","D13","D14","D15","D16",
+                          "T1","T2","T3","T4","T5","T6","T7","T8","T9","T10",
+                          "T11","T12","T13",
+                          "I1","I2","I3","I4",
+                          "Cage Banks", "Cat room"]
+    let patientRecords = UserDefaults.standard.object(forKey: "patientRecords") as? Array<Dictionary<String,String>> ?? []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //let Slayer = CAShapeLayer()
-        //Slayer.opacity = 0.5
-       // Slayer.path = UIBezierPath(roundedRect: CGRect(x: 64, y: 64, width: 100, height: 70), cornerRadius: 5).cgPath
-        //Slayer.fillColor = UIColor.polar().cgColor
-        //Slayer.lineWidth = 2
-        //Slayer.strokeColor = UIColor.backButtonBlue().cgColor
-        
-        //let label = UILabel()
-        //label.font = UIFont(name: "Helvetica-Bold", size: 12)
-        //let center : CGPoint = CGPoint(x: CGRect.midX(Slayer.bounds), y: CGRectGetMidX(Slayer.bounds))
-        //label.text = "Hello"
-        //label.textColor = UIColor.red
-        //label.isHidden = false
-        
-        //mapView.layer.addSublayer(Slayer)
-        
-        //drawRow(startX:200,startY:64, cells:7, cellHeight: 70, cellwidth: 100)
-        //drawRow(startX:200+100+5,startY:64, cells:7, cellHeight: 70, cellwidth: 100)
-        
-        //scrollView.delegate = self
-        //scrollView.minimumZoomScale = 1.0
-        //scrollView.maximumZoomScale = 3.0
-        //scrollView.zoomScale = 1.0
+        singleButtons?.enumerated().forEach({ index, button in
+            button.tag = index
+            button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        })
+        occupiedKennels()
     }
-
-//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        return mapView
-//    }
-//    func drawRow(startX:Int,startY:Int, cells:Int, cellHeight:Int, cellwidth:Int)
-//    {
-//        var newStartY = startY-cellHeight
-//        cells.times {
-//            newStartY += cellHeight
-//            let layer = CAShapeLayer()
-//            layer.path = UIBezierPath(roundedRect: CGRect(x: startX, y: newStartY, width: cellwidth, height: cellHeight), cornerRadius: 5).cgPath
-//            layer.fillColor = UIColor.polar().cgColor
-//            layer.lineWidth = 2
-//            layer.strokeColor = UIColor.backButtonBlue().cgColor
-//            mapView.layer.addSublayer(layer)
-//        }
-//    }
-    
+    @objc func buttonClicked(sender: UIButton?) {
+        let tag = sender?.tag ?? 0
+        singleButtons[tag].backgroundColor = UIColor.candyGreen()
+    }
+    func occupiedKennels(){
+        var tagNo = 0
+        for patient in patientRecords {
+            switch patient["kennelID"]! {
+            case "S1","S2","S3","S4","S5","S6","S7","S8":
+                print("kennelID \(patient["kennelID"]!)")
+                tagNo = (Int(String(patient["kennelID"]!.dropFirst(1)))!)-1
+                print("tagNo \(tagNo)")
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+            case "D1","D2","D3","D4","D5","D6","D7","D8":
+                print("kennelID \(patient["kennelID"]!)")
+                tagNo = ((Int(String(patient["kennelID"]!.dropFirst(1)))!)-1)+8
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+                print("tagNo \(tagNo)")
+            case "D9","D10","D11","D12","D13","D14","D15","D16":
+                print("kennelID \(patient["kennelID"]!)")
+                tagNo = ((Int(String(patient["kennelID"]!.dropFirst(1)))!)-1)+8
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+                print("tagNo \(tagNo)")
+            case "T1","T2","T3","T4","T5","T6","T7","T8","T9","T10","T11","T12","T13":
+                print("kennelID \(patient["kennelID"]!)")
+                tagNo = ((Int(String(patient["kennelID"]!.dropFirst(1)))!)-1)+24
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+                print("tagNo \(tagNo)")
+            case "I1","I2","I3","I4":
+                print("kennelID \(patient["kennelID"]!)")
+                tagNo = ((Int(String(patient["kennelID"]!.dropFirst(1)))!)-1)+37
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+                print("tagNo \(tagNo)")
+            case "Cat room","Cage Banks":
+                tagNo = 41
+                print("kennelID \(patient["kennelID"]!)")
+                singleButtons[tagNo].backgroundColor = UIColor.DarkRed()
+                print("tagNo \(tagNo)")
+            default: tagNo = 0
+            }
+            
+            
+        }
+    }
 }
