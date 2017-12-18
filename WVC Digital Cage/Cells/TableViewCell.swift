607e3e286bb9249106993693ddc4762fd6d17283
brian bird <birdsoftware@gmail.com>
2017-12-18 15:50:02 -0800

    Treatment 3 collection views

--- WVC Digital Cage/Cells/TableViewCell.swift
+++ WVC Digital Cage/Cells/TableViewCell.swift
@@ -60,25 +60,3 @@ class mapDisplayTableView: UITableViewCell {
     
 }
 
-class CategoryRow : UITableViewCell { }
-extension CategoryRow : UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath)
-        return cell
-    }
-}
-//extension CategoryRow : UICollectionViewDelegateFlowLayout {
-//    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let itemsPerRow:CGFloat = 3
-//        let hardCodedPadding:CGFloat = 5
-//        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
-//        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
-//        return CGSize(width: itemWidth, height: itemHeight)
-//    }
-//    
-//}
-
